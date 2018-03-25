e4f6efafbfd3f62ce7daf00584eebaaa3d47848f
j3698 <antiochsanders@gmail.com>
2018-03-25 18:29:27 +0000

    Dump commit

--- client/script.js
+++ client/script.js
@@ -13,28 +13,87 @@ var cardInsides =
   <button class="card-option fancy-button card-delete">Remove</button>
 </div>
 `;
+var names = ["aang", "katara", "sokka", "toph", "zuko", "iroh", "azula",
+             "appa", "momo", "admiral-zhao", "suki", "king-bumi",
+             "princess-yue", "jet", "mai", "ty-lee", "ozai", "roku",
+             "cabbage-man", "aunt-wu", "master-pakku", "jeong-jeong",
+             "combustion-man", "joo-dee"];
+var baseURL = "https://guess-j3698.c9users.io/"
 
 var numRemoved = 0;
 var removed = [];
+var socket;
 
-function ask_q(){
-  var el = document.getElementsByName("to-ask")[0];
-  var q = el.value;
-  $("#asked-text").text(q);
+function askQ() {
+  var el = $("#to-ask");
+  var q = el.val();
+  $("#asked-text").text("Waiting for the other player to ask their question...");
+  $("#answered-text").text("Waiting for the other player to answer your question...");
   $("#asked").css("visibility", "visible");
   console.log("You Asked: " + q);
-  el.value = "";
+  el.val("");
+  askServer(q);
 }
 
-function startCards() {
-  $(".card").append(cardInsides);
+function findCardName(card) {
+  return card.find(".card-img")[0].src.split("/").pop().split(".").shift();
+}
 
-  $(".card").each(function(index, el) {
-    el.click(function(event) {
-      console.log('...');
-      el.addClass('pushed-down');
-    });
+function addDeleteListener() {
+ $(".card-delete").click(function(event) {
+    var curr = $(this);
+    while (!curr.hasClass("card")) { curr = curr.parent(); }
+    curr.find(".option-div").css("visibility", "hidden");
+    curr.find(".remove-div").css("visibility", "hidden");
+    curr.css("visibility", "hidden")
+
+    var name = findCardName(curr);
+    console.log("You Deleted: " + name);
+    curr.find(".option-div").remove();
+    curr.find(".remove-div").remove();
+    curr.find(".guess-div").remove();
+    numRemoved++;
+    removed.push(name);
+    if(numRemoved == 24){
+      alert("Why would you remove all of them =.=");
+    }
+    removeServer(name)
   });
+}
+
+function addChooseListener() {
+  $(".card-choose").click(function(event) {
+    var curr = $(this);
+    while (!curr.hasClass("card")) { curr = curr.parent(); }
+    curr.find(".option-div").css("visibility", "hidden");
+    curr.find(".guess-div").css("visibility", "hidden");
+    var el = curr.find(".card-img");
+    var name = findCardName(curr);
+    console.log("You Chose: " + name);
+    curr.find(".option-div").remove();
+    curr.find(".remove-div").remove();
+    curr.find(".guess-div").remove();
+    var cards = $(".card");
+    for (var i = 0; i < 24; i++) {
+      var curr = $(cards[i]);
+      var currName = findCardName(curr);
+      if (removed.indexOf(currName) < 0) {
+        if (currName != name) {
+          curr.find(".option-div").remove();
+          curr.find(".remove-div").remove();
+          curr.find(".guess-div").remove();
+          curr.css("visibility", "hidden");
+          numRemoved++;
+          removed.push(currName);
+        }
+      }
+    }
+    guessServer(name);
+  });
+}
+
+function setupCardGUI() {
+  $(".card").append(cardInsides);
 
   $(".card").each(function(index, el) {
     $(el).mouseenter(function(event) {
@@ -46,83 +105,43 @@ function startCards() {
       $(el).find(".remove-div").css("visibility", "hidden");
     });
   });
+
   $(".remove-button").click(function(event) {
     var curr = $(this);
-    while (!curr.hasClass("card")) {
-      curr = curr.parent();
-    }
+    while (!curr.hasClass("card")) { curr = curr.parent(); }
     curr.find(".option-div").css("visibility", "hidden");
     curr.find(".remove-div").css("visibility", "visible");
   });
-
+  
   $(".guess-button").click(function(event) {
     var curr = $(this);
-    while (!curr.hasClass("card")) {
-      curr = curr.parent();
-    }
+    while (!curr.hasClass("card")) { curr = curr.parent(); }
     curr.find(".option-div").css("visibility", "hidden");
     curr.find(".guess-div").css("visibility", "visible");
     // curr.find(".guess-button");  TODO :hover isn't activated on guess
   });
- $(".card-delete").click(function(event) {
-    var curr = $(this);
-    while (!curr.hasClass("card")) {
-      curr = curr.parent();
-    }
-    curr.find(".option-div").css("visibility", "hidden");
-    curr.find(".remove-div").css("visibility", "hidden");
-    var el = curr.find(".card-img");
-    var name = el[0].src.substring(43);
-    console.log("You Deleted: " + name);
-    el.attr('src', './pics/black.png');
-    curr.find(".option-div").remove();
-    curr.find(".remove-div").remove();
-    curr.find(".guess-div").remove();
-    numRemoved++;
-    removed.push(el[0].src);
-    if(numRemoved == 24){
-      alert("Why would you remove all of them =.=");
-    }
-  });
-  $(".card-choose").click(function(event) {
-    var curr = $(this);
-    while (!curr.hasClass("card")) {
-      curr = curr.parent();
-    }
-    curr.find(".option-div").css("visibility", "hidden");
-    curr.find(".guess-div").css("visibility", "hidden");
-    var el = curr.find(".card-img");
-    var name = el[0].src;
-    console.log("You Chose: " + name.substring(43));
-    curr.find(".option-div").remove();
-    curr.find(".remove-div").remove();
-    curr.find(".guess-div").remove();
-    var cards = $(".card");
-    for(var i = 0; i < 24; i++){
-      var curr = $(cards[i]);
-      var currName = curr.find(".card-img")[0].src;
-      if(removed.indexOf(currName) < 0){
-        if(currName != name){
-          curr.find(".card-img").attr('src', './pics/black.png');
-          curr.find(".option-div").remove();
-          curr.find(".remove-div").remove();
-          curr.find(".guess-div").remove();
-          numRemoved++;
-          removed.push(currName);
-        }
-      }
-    }
+}
+
+function setupCardSelectGUI(){
+  $(".card-select").each(function(index, el) {
+    $(el).click(function(event) {
+      $("#select").css("visibility", "hidden");
+      var currName = findCardName($(el));
+      console.log("You Picked: " + currName);
+      $(".main-card").find(".card-img").attr('src',"./client/pics/" + currName + ".png");
+      selectServer(currName);
+    });
   });
 }
 
-// $(window).on("load", handler)
-$(document).ready(function() {
-  console.log(window.location);
-  var el = document.getElementById("ask-button");
-  el.onclick = function(){ask_q()};
-  $("#asked").css("visibility", "visible");
-  startCards();
+function startCards() {
+  setupCardGUI();
+  addDeleteListener();
+  addChooseListener();
+  setupCardSelectGUI();
+}
 
+function startSideBar() {
   $("#help-back").click(function() {
     $("#help").css("visibility", "hidden");
   });
@@ -135,30 +154,97 @@ $(document).ready(function() {
   });
   $("#ask-yes").click(function() {
     $("#asked").css("visibility", "hidden");
+    answerServer('yes');
+    $("#answered").css("visibility", "visible");
   });
   $("#ask-no").click(function() {
     $("#asked").css("visibility", "hidden");
+    answerServer('no');
+    $("#answered").css("visibility", "visible");
   });
+  $("#answered-back").click(function() {
+    $("#answered").css("visibility", "hidden");
+    $("#answered-back").prop('disabled', true);
+  });
+}
+
+// $(window).on("load", handler)
+$(document).ready(function() {
+  $("#ask-button").click(askQ);
+  $("#select").css("visibility", "visible");
+  startCards();
+  startSideBar()
   initIO();
 });
 
-// enums
-var IO_EVTS = Object.freeze({
-});
 var GAME_STATE = Object.freeze({
-    START : 'start',
-    PLAY : 'play',
-    OVER_TIE : 'tie',
-    OVER_LOST : 'lost',
-    OVER_WON : 'won'
+  PAIR: 'pair',
+  START : 'start',
+  PLAY : 'play',
+  OVER_TIE : 'tie',
+  OVER_LOST : 'lost',
+  OVER_WON : 'won'
 });
-
-// game start
-var state = GAME_STATE.PLAY;
-
 
 function initIO() {
     socket = io();
-    // socket.on(IO_EVTS.STATE, function(data) {));
-    // socket.emit(IO_EVTS.KEY_DN, action);
+
+    var match = window.location.href.replace(baseURL, "");
+    socket.emit(IO_EVTS.PAIR, match);
+
+    socket.on(IO_EVTS.ASK, function(data) {
+      $("#asked-text").text(data);
+    });
+    
+    socket.on(IO_EVTS.ANSWER, function(data) {
+      console.log(data);
+      $("#answered-text").text(data);
+      $("#answered-back").prop('disabled', false);
+    });
+
+    socket.on(IO_EVTS.REMOVE, function(data) {
+      var index = names.indexOf(data);
+      var miniCard = $($(".mini-card")[index]);
+      miniCard.css("visibility", "hidden");
+    });
+
+    socket.on(IO_EVTS.WIN_STATE, function(data) {
+      console.log(data);
+      
+      $("#winpage").css("visibility", "visible");
+      alert(data);
+      //change later
+    });
 };
+
+// enums
+var IO_EVTS = Object.freeze({
+  PAIR: 'pair',
+  REMOVE: 'remove',
+  GUESS: 'guess',
+  SELECT: 'select',
+  ASK: 'ask',
+  ANSWER: 'answer',
+  WIN_STATE: 'win_state'
+});
+
+
+function removeServer(whomst) {
+  socket.emit(IO_EVTS.REMOVE, whomst);
+}
+
+function guessServer(whomst) {
+  socket.emit(IO_EVTS.GUESS, whomst);
+}
+
+function selectServer(whomst) {
+  socket.emit(IO_EVTS.SELECT, whomst);
+}
+
+function askServer(what) {
+  socket.emit(IO_EVTS.ASK, what);
+}
+
+function answerServer(which) {
+  socket.emit(IO_EVTS.ANSWER, which);
+}
--- index.js
+++ index.js
@@ -1,43 +1,212 @@
-/*
-TODO
-*/
-
-// imports
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var heapq = require('heapq');
 
-
 // static files
-app.get('/', function(req, res) {
+app.use('/client', express.static(__dirname + "/client"));
+
+// index
+app.get('/*', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-// pair up clients
-io.on('connection', function(socket) {
-    if (queue.length) {
-        var pair = queue.shift();
-        var game = new Game(pair, socket);
-        games.add(game);
-    } else {
-        queue.push(socket);
-    }
-});
-app.use(express.static(__dirname + "/client"));
-http.listen(process.env.PORT, process.env.IP);
-
-
+io.on('connection', pairClient);
 
 // enums
 var IO_EVTS = Object.freeze({
+  PAIR: 'pair',
+  REMOVE: 'remove',
+  GUESS: 'guess',
+  SELECT: 'select',
+  ASK: 'ask',
+  ANSWER: 'answer',
+  WIN_STATE: 'win_state'
 });
 
-// variables
+function pairClient(socket) {
+    socket.once(IO_EVTS.PAIR, function(data) {
+        // auto match make
+        if (data == "") {
+            if (queue.length) {
+                var pair = queue.shift();
+                var game = new Game(pair, socket);
+                games.add(game);
+            } else {
+                queue.push(socket);
+            }
+        // match make by url
+        } else {
+            // matched game already at url
+            if (urlPairedGames[data] == 0) {
+                // don't match
+            // no other socket at url
+            } else if (urlPairedGames[data] == null) {
+                urlPairedGames[data] = socket;
+            // socket at url
+            } else {
+                var game = new Game(urlPairedGames[data], socket);
+                games.add(game);
+                urlPairedGames[data] = 0;
+            }
+        }
+    });
+}
+
 var queue = [];
 var games = new Set();
+var urlPairedGames = {};
+
+var names = ["aang", "katara", "sokka", "toph", "zuko", "iroh", "azula",
+             "appa", "momo", "admiral-zhao", "suki", "king-bumi",
+             "princess-yue", "jet", "mai", "ty-lee", "ozai", "roku",
+             "cabbage-man", "aunt-wu", "master-pakku", "jeong-jeong",
+             "combustion-man", "joo-dee"];
 
 function Game(first, second) {
     var game = this;
+    this.firstRemoved = [];
+    this.secondRemoved = [];
+    this.firstSelect;
+    this.secondSelect;
+    var firstQ = "";
+    var secondQ = "";
+    var firstA = "";
+    var secondA = "";
+    // "select" "ask" "wait1/2" "answer"
+    /* gamestates:
+     select - waiting for both players to select a character
+     ask - both players thinking of questions
+     wait1 - only first player has asked a question
+     wait2 - only second player has asked a question
+     answer - 
+    */
+    var gameState = "select";
+    
+    first.on(IO_EVTS.REMOVE, function(data) {
+        if (gameState != "ask" && gameState != "wait2") { return; }
+        if (!isString(data)) { return; }
+        if (game.firstRemoved.indexOf(data) < 0 && names.indexOf(data) >= 0) {
+            game.firstRemoved.push(data);
+            second.emit(IO_EVTS.REMOVE, data);
+        }
+    });
+    second.on(IO_EVTS.REMOVE, function(data) {
+        if (gameState != "ask" && gameState != "wait1") { return; }
+        if (!isString(data)) { return; }
+        if (game.secondRemoved.indexOf(data) < 0 && names.indexOf(data) >= 0) {
+            game.secondRemoved.push(data);
+            first.emit(IO_EVTS.REMOVE, data);
+        }
+    });
+
+    first.on(IO_EVTS.ASK, function(data) {
+        if (gameState != "ask" && gameState != "wait2") { return; }
+        if (!isString(data)) { return; }
+        if(firstQ == ""){
+            firstQ = data;
+            gameState = "wait1";
+            if(secondQ != ""){
+                first.emit(IO_EVTS.ASK, secondQ);
+                second.emit(IO_EVTS.ASK, firstQ);
+                gameState = "answer";
+            }
+        }
+    });
+    second.on(IO_EVTS.ASK, function(data) {
+        if (gameState != "ask" && gameState != "wait1") { return; }
+        if (!isString(data)) { return; }
+        if(secondQ == ""){
+            secondQ = data;
+            gameState = "wait2";
+            if(firstQ != ""){
+                first.emit(IO_EVTS.ASK, secondQ);
+                second.emit(IO_EVTS.ASK, firstQ);
+                gameState = "answer";
+            }
+        }
+    });
+    first.on(IO_EVTS.GUESS, function(data) {
+        if (gameState != "ask" && gameState != "wait2") { return; }
+        if (!isString(data)) { return; }
+        console.log(data + ' ' + game.secondSelect);
+        gameState = "end";
+        if (data == game.secondSelect) {
+            first.emit(IO_EVTS.WIN_STATE, 'win');
+            second.emit(IO_EVTS.WIN_STATE, 'lose');
+        } else {
+            first.emit(IO_EVTS.WIN_STATE, 'lose');
+            second.emit(IO_EVTS.WIN_STATE, 'win');
+        }
+    });
+    second.on(IO_EVTS.GUESS, function(data) {
+        if (gameState != "ask" && gameState != "wait1") { return; }
+        if (!isString(data)) { return; }
+        console.log(data + ' ' + game.firstSelect);
+        gameState = "end";
+        if (data == game.firstSelect) {
+            second.emit(IO_EVTS.WIN_STATE, 'win');
+            first.emit(IO_EVTS.WIN_STATE, 'lose');
+        } else {
+            second.emit(IO_EVTS.WIN_STATE, 'lose');
+            first.emit(IO_EVTS.WIN_STATE, 'win');
+        }
+    });
+
+    first.on(IO_EVTS.SELECT, function(data) {
+        if (gameState != "select") { return; }
+        if (!isString(data)) { return; }
+        game.firstSelect = data;
+        if (game.secondSelect) {
+            gameState = "ask";
+        }
+    });
+    second.on(IO_EVTS.SELECT, function(data) {
+        if (gameState != "select") { return; }
+        if (!isString(data)) { return; }
+        game.secondSelect = data;
+        if (game.firstSelect) {
+            gameState = "ask";
+        }
+    });
+
+    first.on(IO_EVTS.ANSWER, function(data) {
+        if (gameState != "answer") { return; }
+        if (!isString(data)) { return; }
+        firstA = data;
+        if(secondA != ""){
+            var string1 = "You Asked: " + secondQ + '\n' + "They answered: " + firstA;
+            second.emit(IO_EVTS.ANSWER, string1);
+            var string2 = "You Asked: " + firstQ + '\n' + "They answered: " + secondA;
+            first.emit(IO_EVTS.ANSWER, string2);
+            firstQ = "";
+            firstA = "";
+            secondQ = "";
+            secondA = "";
+            gameState = "ask";
+        }
+    });
+    second.on(IO_EVTS.ANSWER, function(data) {
+        if (gameState != "answer") { return; }
+        if (!isString(data)) { return; }
+        secondA = data;
+        if(firstA != ""){
+            var string1 = "You Asked: " + secondQ + " | " + "They answered: " + firstA;
+            second.emit(IO_EVTS.ANSWER, string1);
+            var string2 = "You Asked: " + firstQ + " | " + "They answered: " + secondA;
+            first.emit(IO_EVTS.ANSWER, string2);
+            firstQ = "";
+            firstA = "";
+            secondQ = "";
+            secondA = "";
+            gameState = "ask";
+        }
+    });
 }
+
+function isString(data) {
+    return typeof data === "string" || data instanceof String;
+}
+
+http.listen(process.env.PORT, process.env.IP);
